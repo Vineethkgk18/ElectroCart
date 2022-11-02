93659217ee5ae9f69007b2c7c47895356aa61660
Vineeth krishnan <Vineethkgk18@gmail.com>
2022-11-02 17:58:35 +0530

    modified connections

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 
 const mongoClient=require('mongodb').MongoClient  
-//const env=
 require('dotenv').config()
 const state={
     db:null
@@ -8,11 +7,7 @@ const state={
 
 module.exports.connect=function(done){
     //const url= 'mongodb://localhost:27017'
-    //const url=process.env.db;  
-   // const url= 'mongodb+srv://8714469938:04672284965@cluster0.ibfbseb.mongodb.net/?retryWrites=true&w=majority'
-   //const url= 'mongodb+srv://8714469938:04672284965@cluster0.ibfbseb.mongodb.net/?retryWrites=true&w=majority'
     
-    //const url='mongodb+srv://8714469938:password@cluster0.hesel1m.mongodb.net/test'
     const url=process.env.db;
     const dbname='Electrocart'
     mongoClient.connect(url,{useNewUrlParser:true},(err,data)=>{
